e9e3481372698676ae06da5e33c0b331201b1382
danrlei@outlook.com <124926993+DanrleiAndrade@users.noreply.github.com>
2023-09-25 11:36:20 -0300

    Modificações no código do bancoNode

--- Module_2/JS_Atividades_em_Sala/bancoNode/model/agencia.js
+++ Module_2/JS_Atividades_em_Sala/bancoNode/model/agencia.js
@@ -1,10 +1,56 @@
 export class Agencia{
+
+    #nome;
+    #numero;
+    #telefone;
+    #email;
+    #endereco;
+
     constructor(nome, numero, telefone, email, endereco){
-        this.nome = nome;
-        this.numero = numero;
-        this.telefone = telefone;
-        this.email = email;
-        this.endereco = endereco;
+        this.#nome = nome;
+        this.#numero = numero;
+        this.#telefone = telefone;
+        this.#email = email;
+        this.#endereco = endereco;
     }
 
+    get nome(){
+        return this.#nome;
+    }
+
+    get numero(){
+        return this.#numero;
+    }
+
+    get telefone(){
+        return this.#telefone;
+    }
+
+    get email(){
+        return this.#email;
+    }
+
+    get endereco(){
+        return this.#endereco;
+    }
+
+    set nome(nome){
+        this.#nome = nome;
+    }
+
+    set numero(numero){
+        this.#numero = numero;
+    }
+
+    set telefone(telefone){
+        this.#telefone = telefone;
+    }
+
+    set email(email){
+        this.#email = email;
+    }
+
+    set endereco(endereco){
+        this.#endereco = endereco;
+    }
 }
